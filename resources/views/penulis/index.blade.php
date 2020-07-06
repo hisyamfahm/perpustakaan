b4ae30391d04f1c0b4f1fd1a4a616348cbb5e449
Damayantiatik <66894754+Damayantiatik@users.noreply.github.com>
2020-07-06 14:34:43 +0700

    Add files via upload

--- resources/views/penulis/index.blade.php
+++ resources/views/penulis/index.blade.php
@@ -9,9 +9,9 @@
                             @endif
                 <div class="row">
                 <div class="col-md-12">
-			<!-- TABLE HOVER -->
-			<div class="panel">
-				    <div class="panel-heading">
+            <!-- TABLE HOVER -->
+            <div class="panel">
+                    <div class="panel-heading">
                         <h3 class="panel-title">Data Penulis</h3>
                         @if(Auth::check() && Auth::user()->role =='admin')
                         <div class="right">
@@ -19,48 +19,48 @@
                         </div>
                         @endif  
                     </div> 
-				<div class="panel-body">
+                <div class="panel-body">
                 <form action="{{ url('penulis') }}" method="get">
                     <div class="input-group">
-						<input class="form-control" name="cari" type="text" placeholder="Tulis Nama Penulisnya">
-						<span  class="input-group-btn"><button class="btn btn-primary">Cari</button></span> 
+                        <input class="form-control" name="cari" type="text" placeholder="Tulis Nama Penulisnya">
+                        <span  class="input-group-btn"><button class="btn btn-primary">Cari</button></span> 
                     </div>
                 </form>
                 <br>
-					<table class="table table-hover">
-						<thead>
-							<tr>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
                             <th>No</th>
-                            <th>Nama Depan</th>
-                            <th>Nama Belakang</th>
+                            <th>Nama Penulis</th>
+                             <th>Informasi</th>
                             </tr>
-						</thead>
-						<tbody>
+                        </thead>
+                        <tbody>
                         
                         @foreach($data_penulis as $pk)
                         <tr>
                         <td>{{$pk->id}}</td>
-                            <td>{{$pk->NamaDepan}}</td>
-                            <td>{{$pk->NamaBelakang}}</td>
+                        <td><a href="{{url('buku/penulis/'.$pk->id)}}">{{$pk->NamaDepan}} {{$pk->NamaBelakang}}</a></td>
                             @if(Auth::check() && Auth::user()->role =='admin')
                             <td><a href="{{ url('penulis/'.$pk->id.'/editpenulis') }}" class="btn btn-warning btn-sm" >Ubah</a></td>
                             <td><a href="{{ url('penulis/'.$pk->id.'/hapus') }}" class="btn btn-danger btn-sm" onclick="return confirm('Hapus nih ?')">Hapus</a></td>
                             @endif
+                              <td><a href="{{ url($pk->Informasi) }}" class="btn btn-info btn-sm">Kunjungi</a></td>
                         </tr>
                         @endforeach
                         </tbody>
-					</table>
+                    </table>
                     <b>Halaman :</b>                   <span class="badge">{{ $data_penulis->currentPage() }}</span><br/>
                     <b> Jumlah Data :</b>              <span class="badge"> {{ $data_penulis->total() }} </span><br/>
                     <b> Data Per Halaman : </b>         <span class="badge">  {{ $data_penulis->perPage() }}</span> <br/>
                 
                 
                     {{ $data_penulis->links() }}
-				</div>
-			</div>
-			<!-- END TABLE HOVER -->
+                </div>
+            </div>
+            <!-- END TABLE HOVER -->
 
-		</div>
+        </div>
 
                 </div>
             </div>
